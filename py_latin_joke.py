1e8349a573d01997d5fe5d79806d3aa8e5a45c08
sandrabosk <sandraboskovic@hotmail.com>
2017-10-16 16:04:30 -0400

    done

--- py_latin_joke.py
+++ py_latin_joke.py
@@ -1,3 +1,13 @@
+'''  
+Pig Latin is a language game, where you move the first letter of the word to the end and add "ay." So "Python" becomes "ythonpay." To write a Pig Latin translator in Python, here are the steps we'll need to take:
+
+1.	Ask the user to input a word in English.
+2.	Make sure the user entered a valid word.
+3.	Convert the word from English to Pig Latin.
+4.	Display the translation result.
+'''
+
+
 pyg = 'ay'
 
 original = raw_input('Enter a word:')
