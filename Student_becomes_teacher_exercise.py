34b788ee12ab3d9605eddefc6171ae4625a2c331
sandrabosk <sandraboskovic@hotmail.com>
2017-11-04 13:02:53 -0400

    average function

--- Student_becomes_teacher_exercise.py
+++ Student_becomes_teacher_exercise.py
@@ -41,4 +41,29 @@ for student in students:
   print "quizzes scores: %s" %student["quizzes"]
   print "test scores: %s" %student["tests"]
   print
+
+
+''' Write a function average that takes a list of numbers and returns the average:
+    
+    1. Define a function called average that has one argument, numbers.
+    2. Inside that function, call the built-in sum() function with the numbers list as a parameter. Store the result in a variable called total.
+    3. Like the example above, use float() to convert total and store the result in total.
+    4. Divide total by the length of the numbers list. Use the built-in len() function to calculate that.
+    5. Return that result.
+    '''
+
+def average(numbers):
+    total = sum(numbers)
+    total = float(total)
+    return total / len(numbers)
+
+''' Write a function called get_average that takes a student dictionary (like lloyd, alice, or tyler) as input and returns his/her weighted average. Define a function called get_average that takes one argument called student. Make a variable homework that stores the average() of student["homework"]. Repeat the above step for "quizzes" and "tests". Multiply the 3 averages by their weights and return the sum of those three. Homework is 10%, quizzes are 30% and tests are 60%. '''
+
+def get_average(student):
+    homework = average(student["homework"])
+    quizzes = average(student["quizzes"])
+    tests = average(student["tests"])
+                
+    total = homework *.1 + quizzes * .3 + tests * .6
+    return total
   
